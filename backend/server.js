467ea767164cc4424054f46902d29faafd16f044
Vivek Salwan <viveksalwan63@gmail.com>
2024-10-11 11:47:41 +0530

    frontend origin netlify now

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin:'https://roxiler-task-frontend.netlify.app',
   methods: ['GET', 'POST', 'PUT'],
   credentials: true
 }));
